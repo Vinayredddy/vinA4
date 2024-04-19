9f926ae5d54d6946a0197febf81ce8c697bfd8ce
Vinay Reddy <84946322+Vinayredddy@users.noreply.github.com>
2024-04-19 10:54:17 -0500

    Add files via upload

---
+++ key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://gooyou1999:ABC123@cluster0.sibk48c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
